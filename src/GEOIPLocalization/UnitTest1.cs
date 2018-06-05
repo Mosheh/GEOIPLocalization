92f86a45ae43869ddf064dd15e5b829fd6ae44c3
unknown <moisesjosemiranda@gmail.com>
2018-06-05 16:26:22 -0300

    new test case

--- src/GEOIPLocalization/UnitTest1.cs
+++ src/GEOIPLocalization/UnitTest1.cs
@@ -10,22 +10,22 @@ namespace GEOIPLocalization.Tests
         [TestMethod]
         public void TestGoogleLocalization()
         {
-            var obj = new LocalizationService().GetLocalization("8.8.8.8");
-            Assert.AreEqual("California", obj.Region);
+            var localization = new LocalizationService().GetLocalization("8.8.8.8");
+            Assert.AreEqual("California", localization.Region);
         }
 
         [TestMethod]
-        public void TestGoianiaLocalization()
+        public void TestGoianiaBrazilLocalization()
         {
-            var obj = new LocalizationService().GetLocalization();
-            Assert.AreEqual("Goiânia", obj.City);
+            var localization = new LocalizationService().GetLocalization();
+            Assert.AreEqual("Goiânia", localization.City);
         }
 
         [TestMethod]
         public void TestRioDeJaneiroLocalization()
         {
-            var obj = new LocalizationService().GetLocalization("186.192.90.5");
-            Assert.AreEqual("Rio de Janeiro", obj.City);
+            var localization = new LocalizationService().GetLocalization("186.192.90.5");
+            Assert.AreEqual("Rio de Janeiro", localization.City);
         }
     }
 }
